81539128a554c56fec1a267f972b31f83c26b4eb
dinalailil <dinarullailil26@gmail.com>
2025-09-17 16:08:38 +0700

    fungsi faktorial

--- bin/Pertemuan3perulangan/jobsheet3.dart
+++ bin/Pertemuan3perulangan/jobsheet3.dart
@@ -1,3 +1,17 @@
 void main() {
   print("=== Program Percabangan & Perulangan ===");
-} 
+
+  // Hitung faktorial dari 5
+  int n = 5;
+  int hasilFaktorial = faktorial(n);
+  print("\nFaktorial dari $n adalah $hasilFaktorial");
+}
+
+// Fungsi faktorial dengan perulangan for
+int faktorial(int n) {
+  int hasil = 1;
+  for (int i = 1; i <= n; i++) {
+    hasil *= i;
+  }
+  return hasil;
+}
